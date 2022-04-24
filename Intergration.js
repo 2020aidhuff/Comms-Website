b646a0278614f65d1b4205f4c270a8fb0d823bce
2020aidhuff <Aidanhuff2020@gmail.com>
2022-04-24 11:37:32 -0400

    Inputs get saved as Var

---
+++ Intergration.js
@@ -0,0 +1,6 @@
+function inputs()
+	{
+		var node = document.getElementById("nodeID").value;
+		var command = document.getElementById("command").value;
+		alert("Node: "+ node + " " + "Command: " + command );
+	}	
