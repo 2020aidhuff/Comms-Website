2e3f2b1a25230f1377884195b2d597d07e5401c8
2020aidhuff <Aidanhuff2020@gmail.com>
2022-04-24 12:44:28 -0400

    Revert "Inputs get saved as Var"
    This reverts commit b646a0278614f65d1b4205f4c270a8fb0d823bce.

--- Intergration.js
+++
@@ -1,6 +0,0 @@
-function inputs()
-	{
-		var node = document.getElementById("nodeID").value;
-		var command = document.getElementById("command").value;
-		alert("Node: "+ node + " " + "Command: " + command );
-	}	
